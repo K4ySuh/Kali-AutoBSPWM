be48d4493a5b99fb45a8a59ebfa0b3c8b56f26e4
KaySuh <147923141+K4ySuh@users.noreply.github.com>
2023-12-10 14:19:03 +0100

    Delete config/polybar/shapes directory

--- config/polybar/shapes/scripts/vpn_status.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-IFACE=$(/usr/sbin/ifconfig | grep tun0 | awk '{print $1}' | tr -d ':')
-
-if [ "$IFACE" = "tun0" ]; then
-	echo "%{F#ffffff}  %{F#ffffff}$(/usr/sbin/ifconfig tun0 | grep "inet " | awk '{print $2}')%{u-}"
-else
-	echo "%{F#ffffff} %{u-} Disconnected"
-fi
