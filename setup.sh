71925ac3c5b0028b24007442c6862d53125ee511
KaySuh <147923141+K4ySuh@users.noreply.github.com>
2023-12-10 14:41:16 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -179,24 +179,22 @@ else
 
 	cd ..
 
-	echo -e "\n${purpleColour}[*] Installing Oh My Zsh and Powerlevel10k for user $user...\n${endColour}"
+	echo -e "\n${purpleColour}[*] Installing Powerlevel10k for user $user...\n${endColour}"
 	sleep 2
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 	if [ $? != 0 ] && [ $? != 130 ]; then
-		echo -e "\n${redColour}[-] Failed to install Oh My Zsh and Powerlevel10k for user $user!\n${endColour}"
+		echo -e "\n${redColour}[-] Failed to install Powerlevel10k for user $user!\n${endColour}"
 		exit 1
 	else
 		echo -e "\n${greenColour}[+] Done\n${endColour}"
 		sleep 1.5
 	fi
 
-	echo -e "\n${purpleColour}[*] Installing Oh My Zsh and Powerlevel10k for user root...\n${endColour}"
+	echo -e "\n${purpleColour}[*] Installing Powerlevel10k for user root...\n${endColour}"
 	sleep 2
-	sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 	sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /root/.oh-my-zsh/custom/themes/powerlevel10k
 	if [ $? != 0 ] && [ $? != 130 ]; then
-		echo -e "\n${redColour}[-] Failed to install Oh My Zsh and Powerlevel10k for user root!\n${endColour}"
+		echo -e "\n${redColour}[-] Failed to install Powerlevel10k for user root!\n${endColour}"
 		exit 1
 	else
 		echo -e "\n${greenColour}[+] Done\n${endColour}"
