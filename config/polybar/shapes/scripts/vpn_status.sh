b8326b05e1c9c2988a849078f2e13665f1323d62
KaySuh <147923141+K4ySuh@users.noreply.github.com>
2024-03-05 19:21:12 +0100

    Updated Polybar so now is smoother

---
+++ config/polybar/shapes/scripts/vpn_status.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+greenColour="\e[0;32m\033[1m"
+endColour="\033[0m\e[0m"
+redColour="\e[0;31m\033[1m"
+blueColour="\e[0;34m\033[1m"
+yellowColour="\e[0;33m\033[1m"
+purpleColour="\e[0;35m\033[1m"
+turquoiseColour="\e[0;36m\033[1m"
+grayColour="\e[0;37m\033[1m"
+
+IFACE=$(/usr/sbin/ifconfig | grep tun0 | awk '{print $1}' | tr -d ':')
+
+if [ "$IFACE" = "tun0" ]; then
+	echo "%{F#ffffff}  %{F#ffffff}$(/usr/sbin/ifconfig tun0 | grep "inet " | awk '{print $2}')%{u-}"
+else
+	echo "%{F#ffffff} %{u-} Disconnected"
+fi
